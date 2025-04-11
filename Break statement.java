0256cc3df642ae8c8680fc88f4490e213d5eb9c4
Abinaya <abivijayakumar419@gmail.com>
2025-04-11 22:16:29 +0530

    Create Break statement.java

---
+++ Break statement.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Main{
+  public static void main(String[]args)
+  {
+    Scanner scan = new Scanner(System.in);
+    int day= scan.nextInt();
+    switch (4){
+      case 1:
+        System.out.println("sunday");
+        break;
+      case 2:
+        System.out.println("Monday");
+        break;
+      case 3:
+        System.out.println("Tuesday");
+        break;
+      case 4:
+        System.out.println("Wednesday");
+        break;
+      case 5:
+        System.out.println("Thursday");
+        break;
+      case 6:
+        System.out.println("Friday");
+        break;
+      case 7:
+        System.out.println("saturday");
+        break;
+      case 8:
+        System.out.println("default");
+       break;
+    }
+  }
+}
+        
+        
+      
