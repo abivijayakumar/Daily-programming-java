22a27270895eaa25ea1e9d4c4e0c0f3bdedbe545
Abinaya <abivijayakumar419@gmail.com>
2025-04-12 07:08:46 +0530

    Create conditional statement.java

---
+++ conditional statement.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main{
+  public class static void main(String[]args)
+  int n=15;
+  if((n%3==0)&&(n%5)==0))
+{
+  System.out.println("Both 3&5");
+}
+else if(n%3=0)
+{
+  System.out.println("Div by 3");
+}
+else if(n%5==0)
+{
+  System.out.println("Div by 5");
+}
