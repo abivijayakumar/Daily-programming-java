876488a79c841b53ae501d9a4a8af62f05bcf917
Abinaya <abivijayakumar419@gmail.com>
2025-04-13 07:19:15 +0530

    Create Leap year.java

---
+++ Leap year.java
@@ -0,0 +1,15 @@
+class Main
+  {
+    public static void main(String[]args)
+    {
+      int n=2025;
+      if(n%400==0)
+      {
+        System.out.println("It is a leap year");
+      }
+      else
+      {
+        System.out.println("It is not a leap year");
+      }
+    }
+  }
