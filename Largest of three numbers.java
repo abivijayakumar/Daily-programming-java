e1a1b28f9cf22c8bbc3a9c456e6e5d89c2077625
Abinaya <abivijayakumar419@gmail.com>
2025-05-14 07:49:50 +0530

    Create Largest of three numbers.java

---
+++ Largest of three numbers.java
@@ -0,0 +1,6 @@
+public class Largest {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 15;
+        System.out.println("Largest: " + Math.max(a, Math.max(b, c)));
+    }
+}
