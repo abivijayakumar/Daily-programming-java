b86ca329d10e0c35d93e645a49f400c90cd27910
Abinaya <abivijayakumar419@gmail.com>
2025-07-22 20:55:57 +0530

    Create program.java

---
+++ program.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class program{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int a=n;a>=1;a=a-1){
+      System.outprintln("Number"+a);
+    }
+  }
+}
