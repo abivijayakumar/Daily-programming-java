41515f851295e5d6523130105b68af3ec691cbf1
Abinaya <abivijayakumar419@gmail.com>
2025-09-13 15:57:35 +0530

    Create ReverseOrder.java

---
+++ ReverseOrder.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+  public static void reverseWord(String str){
+    String[] words = str.split(" ");
+    for(int i = words.length -1,i >= 0;i--){
+      System.out.print(words[i]);
+      if(i > 0){
+        System.out.print(" ");
+      }
+    }
+  }
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    String input = scan.nextLine();
+    reverseWord(input);
+  }
+}
