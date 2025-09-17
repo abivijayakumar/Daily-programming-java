2dedc4d2e4b3f37b3b2ca6bf93588b911a37d3d4
Abinaya <abivijayakumar419@gmail.com>
2025-09-17 21:55:58 +0530

    Create loops Multiplication.java

---
+++ loops Multiplication.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    scan.close();
+    for(int i = 1;i <=10;i++){
+      System.out.println(N + " x " + i + " = " + (N * i));
+    }
+  }
+}
