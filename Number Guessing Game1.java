a9b03dd3d5e87b43e8f53c8248cc00f3df2969ab
Abinaya <abivijayakumar419@gmail.com>
2025-09-27 11:13:43 +0530

    Create Number Guessing Game1.java

---
+++ Number Guessing Game1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    Random rand = new Random();
+    int numberToGuess = rand.nextInt(bound 100) + 1;
+    int guess = 0;
+    int attempts = 0;
+    System.out.println("Welcome to the Number Guessing Game");
+    System.out.println("Guess a number between 1 and 100:");
+    while(guess != numberToGuess){
+      System.out.print("Enter your guess: ");
+      guess = scan.nextInt();
+      attempts++;
+      if(guess < numberToGuess){
+        System.out.println("Too low Try again.");
+      } else if (guess > numberToGuess){
+        System.out.println("Too high Try again.");
+      } else {
+        System.out.println("Congratulations! you guessed the number in " + attempts + " attempts.");
+      }
+    }
+    scan.close();
+  }
+}
