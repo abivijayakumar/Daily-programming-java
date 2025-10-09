4d7e8444da1099f988173a1c52ea433ca8a616f7
Abinaya <abivijayakumar419@gmail.com>
2025-10-09 22:23:59 +0530

    Create digit Reverse.java

---
+++ digit Reverse.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number reverse:");
+    int number = scanner.nextInt();
+    int reverse = 0;
+    System.out.println("The reversed number is : " + reverse);
+    scanner.close();
+  }
+}
